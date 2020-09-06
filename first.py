da4f6d52c25eec60708e4d2d748bce6f31298739
Rahul Bali <rahulrdb18@gmail.com>
2020-09-07 00:18:08 +0530

    Update first.py

--- first.py
+++ first.py
@@ -4,7 +4,8 @@ from time import sleep
 from selenium import webdriver
 # driver.set_window_size(1120, 550)
 
-s = "cry"
+s = input()
+dirr = input()
 
 option = webdriver.ChromeOptions()
 chrome_prefs = {}
@@ -13,11 +14,11 @@ chrome_prefs["profile.default_content_settings"] = {"images": 2}
 chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
 option.headless = True
 
+############ change the below path to the chromedriver in your PC ####################
 driver = webdriver.Chrome("D:\libtools\chromedriver\chromedriver.exe", options=option)
 
 
 driver.get("https://unsplash.com/s/photos/"+s)
-
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 sleep(20)
 
@@ -26,8 +27,9 @@ photos = driver.find_elements_by_class_name('_2Mc8_')
 images = [one_photo.get_attribute('href') + "/download?force=true" for one_photo in photos]
 print("Found {} images".format(len(images)))
 
-os.makedirs("photos/" + s)
-os.chdir("photos/" + s)
+# os.makedirs("photos/" + s)
+# os.chdir("photos/" + s)
+os.chdir(dirr)
 
 for count, x in enumerate(images):
     os.system("aria2c -o {:04d} {}".format(count, x))
@@ -38,5 +40,5 @@ for f in files:
     if not f.endswith(".jpg"):
         os.rename(f, f + ".jpg")
 
-print("\nFiles downloaded into photos/{}".format(s))
+print("\nFiles downloaded into {}".format(dir))
 
