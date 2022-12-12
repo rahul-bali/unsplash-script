5fec00d3438e4eab7f7ee09b30f5b3776682771e
rahul <rahulrdb18@gmail.com>
2022-12-13 00:13:32 +0530

    updated code that works

--- unsplash-threaded.py
+++ unsplash-threaded.py
@@ -3,30 +3,34 @@ import os
 import time
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 
 # driver.set_window_size(1120, 550)
 
-s = input()
-# dirr = input()
+s = input("type your search term and hit enter: ")
 
-option = webdriver.ChromeOptions()
-chrome_prefs = {}
-option.experimental_options["prefs"] = chrome_prefs
-chrome_prefs["profile.default_content_settings"] = {"images": 2}
-chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
-option.headless = True
+chrome_options = Options()
+# chrome_options.add_argument(f"--user-data-dir=./chrome-data")
+chrome_options.add_experimental_option("useAutomationExtension", False)
+# chrome_options.headless = True
 
-############ change the below path to the chromedriver in your PC ####################
-driver = webdriver.Chrome("/home/nightshade/bin/chromedriver", options=option)
+service = Service('D:\\libtools\\chromedriver')
 
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://unsplash.com/s/photos/"+s)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 sleep(20)
 
-photos = driver.find_elements_by_class_name('_2Mc8_')
+photos = driver.find_elements(by=By.CLASS_NAME, value='NP4SP')
+
+print(len(photos), photos[0].tag_name)
+
 
 images = [one_photo.get_attribute('href') + "/download?force=true" for one_photo in photos]
 print("Found {} images".format(len(images)))
@@ -36,7 +40,7 @@ os.chdir("photos/" + s)
 # os.chdir(dirr)
 
 def download_image(count, x):
-	os.system("aria2c -o {:04d} {}".format(count, x))
+	os.system("D:\\libtools\\aria2c -o {:04d} {}".format(count, x))
 	print("Downloaded {}".format(x))
 
 with ThreadPoolExecutor() as executor:
